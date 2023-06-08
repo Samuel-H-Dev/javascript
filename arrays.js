8f7f34d6d0c6d9d87751688b9bc7eac169fb7ad2
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-06-08 12:00:42 -0400

    CREATED Arrays and Objects

--- arrays.js
+++ arrays.js
@@ -3,3 +3,18 @@ const students = ['Sam', 'Cihan', 'Ariana', 'Yaslin', 'Kevin'];
 console.log(students[2]);
 
 console.log(students.length);
+
+students.push('Lorenzo');
+students.unshift('Andrew')
+
+console.log(students);
+
+////lets create an array of objects
+
+const courses = [
+    {title: "software Enginering", type: 'fulltime', weeks: 10},
+    {title: "Game Dev", type: "part-time", weeks: 3},
+    {title: "Data Analytics", type: "part-Time", weeks: 3 }
+]
+
+console.log(courses[1].title)
